7a63f255859ef69e79ea8285fd16643ff78feda7
Constellation <utatane.tea@gmail.com>
2011-06-11 06:49:08 +0900

    add LineTerminator pattern to Line infomation

--- src/structured_source.h
+++ src/structured_source.h
@@ -5,13 +5,23 @@
 #include <vector>
 #include <algorithm>
 #include <iv/detail/cstdint.h>
+#include <iv/detail/tuple.h>
 #include <iv/ustringpiece.h>
 #include <iv/character.h>
 namespace az {
 
 class StructuredSource : private iv::core::Noncopyable<StructuredSource> {
  public:
-  typedef std::pair<std::size_t, std::size_t> Line;  // start position and length
+  enum LTType {
+    NONE = 0,
+    CR,
+    LF,
+    U2028,
+    U2029,
+    CRLF,
+    LFCR
+  };
+  typedef std::tuple<std::size_t, std::size_t, LTType> Line;  // start position and length
   typedef std::vector<Line> Lines;
 
   explicit StructuredSource(const iv::core::UStringPiece& str)
@@ -22,21 +32,32 @@ class StructuredSource : private iv::core::Noncopyable<StructuredSource> {
     for (iv::core::UStringPiece::const_iterator it = str.begin(),
          last = str.end(); it != last; ++it, ++pos) {
       if (iv::core::character::IsLineTerminator(*it)) {
-        iv::core::UStringPiece::const_iterator next_it = it;
-        ++next_it;
-        if (next_it != last) {
-          if (*it + *next_it == '\n' + '\r') {
-            it = next_it;
-            ++pos;
+        const uint16_t ch = *it;
+
+        // ECMA262 LineTerminators
+        LTType type =
+            (ch == '\n') ? LF :
+            (ch == '\r') ? CR :
+            (ch == 0x2028) ? U2028 : U2029;
+
+        if (type == LF || type == CR) {
+          iv::core::UStringPiece::const_iterator next_it = it;
+          ++next_it;
+          if (next_it != last) {
+            if (ch + *next_it == '\n' + '\r') {
+              type = (type == LF) ? LFCR : CRLF;
+              it = next_it;
+              ++pos;
+            }
           }
         }
-        source_.push_back(std::make_pair(line_start_pos, std::distance(line_start_it, it + 1)));
+        source_.push_back(std::make_tuple(line_start_pos, std::distance(line_start_it, it + 1), type));
         line_start_pos = pos + 1;
         line_start_it = it + 1;
       }
     }
     if (line_start_it != str.end()) {
-      source_.push_back(std::make_pair(line_start_pos, std::distance(line_start_it, str.end())));
+      source_.push_back(std::make_tuple(line_start_pos, std::distance(line_start_it, str.end()), NONE));
     }
   }
 
@@ -46,15 +67,15 @@ class StructuredSource : private iv::core::Noncopyable<StructuredSource> {
 
   struct LowerBoundFinder {
     bool operator()(const Line& lhs, std::size_t rhs) const {
-      return lhs.first < rhs;
+      return std::get<0>(lhs) < rhs;
     }
 
     bool operator()(const Line& lhs, const Line& rhs) const {
-      return lhs.first < rhs.first;
+      return std::get<0>(lhs) < std::get<0>(rhs);
     }
 
     bool operator()(std::size_t lhs, const Line& rhs) const {
-      return lhs < rhs.first;
+      return lhs < std::get<0>(rhs);
     }
   };
 
@@ -62,14 +83,14 @@ class StructuredSource : private iv::core::Noncopyable<StructuredSource> {
     if (source_.empty()) {
       return std::make_pair(1, pos + 1);
     }
-    // Lines first is always 0
+    // Lines <0> is always 0
     Lines::const_iterator it =
         std::upper_bound(source_.begin(), source_.end(), pos, LowerBoundFinder());
     std::advance(it, -1);
     assert(it != source_.end());
     return std::make_pair(
         std::distance(source_.begin(), it) + 1,
-        (pos + 1) - it->first);
+        (pos + 1) - std::get<0>(*it));
   }
 
  private:
