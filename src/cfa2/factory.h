6b7ea2f470884cb53817f6344f459817d0253a50
Constellation <utatane.tea@gmail.com>
2011-07-29 14:41:40 +0900

    add more template...

---
+++ src/cfa2/factory.h
@@ -0,0 +1,37 @@
+// AObject factory
+#ifndef _AZ_CFA2_FACTORY_H_
+#define _AZ_CFA2_FACTORY_H_
+#include <algorithm>
+#include <iv/noncopyable.h>
+#include "deleter.h"
+#include "cfa2/fwd.h"
+#include "cfa2/aobject.h"
+namespace az {
+namespace cfa2 {
+
+class Factory : private iv::core::Noncopyable<Factory> {
+  Factory()
+    : created_() {
+  }
+
+  AObject* New() {
+    AObject* obj = new AObject();
+    created_.push_back(obj);
+    return obj;
+  }
+
+  AObject* New(FunctionLiteral* constructor, AVal proto) {
+    AObject* obj = new AObject(constructor, proto);
+    created_.push_back(obj);
+    return obj;
+  }
+
+  ~Factory() {
+    std::for_each(created_.begin(), created_.end(), Deleter());
+  }
+
+  std::vector<AObject*> created_;
+};
+
+} }  // namespace az::cfa2
+#endif  // _AZ_CFA2_FACTORY_H_
---
+++ src/cfa2/fwd.h
@@ -0,0 +1,14 @@
+#ifndef _AZ_CFA2_FWD_H_
+#define _AZ_CFA2_FWD_H_
+namespace az {
+namespace cfa2 {
+
+class Factory;
+
+class AObject;
+class AVal;
+class AProp;
+
+class Interpreter;
+} }  // namespace az::cfa2
+#endif  // _AZ_CFA2_FWD_H_
