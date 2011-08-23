cbf4ce0655b18b624f7a24afd796cf577e95dbc1
Constellation <utatane.tea@gmail.com>
2011-08-23 10:41:44 +0900

    implement unwrap comment and tag parsing in JSDoc

--- az/jsdoc/lexer.h
+++ az/jsdoc/lexer.h
@@ -1,6 +1,8 @@
 #ifndef _AZ_JSDOC_LEXER_H_
 #define _AZ_JSDOC_LEXER_H_
+#include <iterator>
 #include <iv/noncopyable.h>
+#include <az/debug_log.h>
 #include <az/jsdoc/token.h>
 #include <az/jsdoc/tag.h>
 namespace az {
@@ -9,10 +11,15 @@ namespace jsdoc {
 class Lexer : private iv::core::Noncopyable<Lexer> {
  public:
   explicit Lexer(const iv::core::UStringPiece& src)
-    : source_(src),
+    : source_(),
       c_(-1),
+      title_(),
+      content_(),
       pos_(0),
-      end_(src.size()) {
+      end_() {
+    source_.reserve(src.size() - 5);
+    Lexer::UnwrapComment(src, std::back_inserter(source_));
+    end_ = source_.size();
     Advance();
   }
 
@@ -49,29 +56,114 @@ class Lexer : private iv::core::Noncopyable<Lexer> {
 
   Token::Type ScanTag() {
     assert(c_ == '@');
-    buffer_.clear();
-    buffer_.push_back('@');
+    ScanTitle();
+    ScanContent();
+    return token_;
+  }
+
+  void ScanTitle() {
+    assert(c_ == '@');
+    title_.clear();
+    // parse title string
+    title_.push_back('@');
     Advance();
     while (c_ >= 0 && iv::core::character::IsASCIIAlphanumeric(c_)) {
-      buffer_.push_back(c_);
+      title_.push_back(c_);
       Advance();
     }
+    assert(c_ < 0 || !iv::core::character::IsASCIIAlphanumeric(c_));
+
     std::size_t index = 0;
+    token_ = Token::TK_TAG;
     for (TagArray::const_iterator it = kKnownTags.begin(),
          last = kKnownTags.end(); it != last; ++it, ++index) {
-      if (it->size() == buffer_.size() &&
-          std::equal(it->begin(), it->end(), buffer_.begin())) {
-        return static_cast<Token::Type>(index);
+      if (it->size() == title_.size() &&
+          std::equal(it->begin(), it->end(), title_.begin())) {
+        token_ = static_cast<Token::Type>(index);
+        break;
       }
     }
-    // not known tag
-    return Token::TK_TAG;
+  }
+
+  void ScanContent() {
+    content_.clear();
+    bool waiting_next_tag = false;
+    while (c_ >= 0) {
+      if (iv::core::character::IsLineTerminator(c_)) {
+        // Line Terminator found
+        waiting_next_tag = true;
+      } else {
+        if (waiting_next_tag) {
+          if (c_ == '@') {
+            // next tag found
+            break;
+          }
+          if (!iv::core::character::IsWhiteSpace(c_)) {
+            waiting_next_tag = false;
+          }
+        }
+      }
+      content_.push_back(c_);
+      Advance();
+    }
+  }
+
+  template<typename OutputIter>
+  static void UnwrapComment(const iv::core::UStringPiece& src,
+                            OutputIter result) {
+    // JSDoc comment is following form
+    //   /**
+    //    * ........
+    //    */
+    // so, remove /**, */ and *.
+    assert(src.size() >= 5);
+    assert(src[0] == '/' && src[1] == '*' && src[2] == '*');
+    assert(src[src.size() - 1] == '/' && src[src.size() - 2] == '*');
+    // remove pre/postfix slash star
+    const iv::core::UStringPiece target(src.data() + 3, src.size() - 5);
+
+    // remove line prefix slash star
+    // mode indicates
+    //      * 
+    //   0 |1| 2
+    int mode = 0;
+    for (iv::core::UStringPiece::const_iterator it = target.begin(),
+         last = target.end(); it != last; ++it) {
+      if (mode == 0) {
+        if (iv::core::character::IsLineTerminator(*it)) {
+          // waste
+          *result++ = *it;
+        } else if (iv::core::character::IsWhiteSpace(*it)) {
+          // waste
+        } else if (*it == '*') {
+          // waste and change mode 1
+          mode = 1;
+        } else {
+          // get and change mode 2
+          mode = 2;
+          *result++ = *it;
+        }
+      } else if (mode == 1) {
+        if (!iv::core::character::IsWhiteSpace(*it)) {
+          *result++ = *it;
+        }
+        mode = 2;
+      } else {
+        assert(mode == 2);
+        *result++ = *it;
+        if (iv::core::character::IsLineTerminator(*it)) {
+          mode = 0;
+        }
+      }
+    }
   }
 
  private:
-  iv::core::UStringPiece source_;
+  std::vector<uint16_t> source_;
   int c_;
-  std::vector<char> buffer_;
+  Token::Type token_;
+  std::vector<char> title_;
+  std::vector<uint16_t> content_;
   std::size_t pos_;
   std::size_t end_;
 };
--- test/test_jsdoc_lexer.cc
+++ test/test_jsdoc_lexer.cc
@@ -3,7 +3,53 @@
 #include <iv/ustring.h>
 #include <az/jsdoc/lexer.h>
 
-TEST(JSDocLexerCase, ConstTest) {
+TEST(JSDocLexerCase, UnwrapCommentTest) {
+  using az::jsdoc::Token;
+  {
+    const iv::core::UString str = iv::core::ToUString(
+        "/**\n"
+        " * @const\n"
+        " * @const\n"
+        " */");
+    std::string result;
+    az::jsdoc::Lexer::UnwrapComment(str, std::back_inserter(result));
+    EXPECT_EQ("\n"
+              "@const\n"
+              "@const\n", result);
+  }
+  {
+    const iv::core::UString str = iv::core::ToUString("/**x*/");
+    std::string result;
+    az::jsdoc::Lexer::UnwrapComment(str, std::back_inserter(result));
+    EXPECT_EQ("x", result);
+  }
+  {
+    const iv::core::UString str = iv::core::ToUString("/***x*/");
+    std::string result;
+    az::jsdoc::Lexer::UnwrapComment(str, std::back_inserter(result));
+    EXPECT_EQ("x", result);
+  }
+  {
+    const iv::core::UString str = iv::core::ToUString("/****x*/");
+    std::string result;
+    az::jsdoc::Lexer::UnwrapComment(str, std::back_inserter(result));
+    EXPECT_EQ("*x", result);
+  }
+  {
+    const iv::core::UString str = iv::core::ToUString("/**x\n * y\n*/");
+    std::string result;
+    az::jsdoc::Lexer::UnwrapComment(str, std::back_inserter(result));
+    EXPECT_EQ("x\ny\n", result);
+  }
+  {
+    const iv::core::UString str = iv::core::ToUString("/**x\n *   y\n*/");
+    std::string result;
+    az::jsdoc::Lexer::UnwrapComment(str, std::back_inserter(result));
+    EXPECT_EQ("x\n  y\n", result);
+  }
+}
+
+TEST(JSDocLexerCase, TagParseTest) {
   using az::jsdoc::Token;
   {
     const iv::core::UString str = iv::core::ToUString("/** @const */");
@@ -16,12 +62,13 @@ TEST(JSDocLexerCase, ConstTest) {
     for (Token::Type token = lexer.Next();
          token != Token::TK_EOS;
          token = lexer.Next(), ++ex) {
-      ASSERT_EQ(*ex, token);
+      EXPECT_EQ(*ex, token);
     }
   }
+
   {
     const iv::core::UString str =
-        iv::core::ToUString("/**@const @const*/");
+        iv::core::ToUString("/**@const\n @const*/");
     az::jsdoc::Lexer lexer(str);
     const std::array<Token::Type, 3> expected = { {
       Token::TK_CONST,
@@ -32,7 +79,30 @@ TEST(JSDocLexerCase, ConstTest) {
     for (Token::Type token = lexer.Next();
          token != Token::TK_EOS;
          token = lexer.Next(), ++ex) {
-      ASSERT_EQ(*ex, token);
+      EXPECT_EQ(*ex, token);
+    }
+  }
+
+  {
+    const iv::core::UString str =
+        iv::core::ToUString(
+            "/**\n"
+            " * @const @const\n"
+            " * @const @const\n"
+            " * @const @const\n"
+            " */");
+    az::jsdoc::Lexer lexer(str);
+    const std::array<Token::Type, 4> expected = { {
+      Token::TK_CONST,
+      Token::TK_CONST,
+      Token::TK_CONST,
+      Token::TK_EOS
+    } };
+    std::array<Token::Type, 3>::const_iterator ex = expected.begin();
+    for (Token::Type token = lexer.Next();
+         token != Token::TK_EOS;
+         token = lexer.Next(), ++ex) {
+      EXPECT_EQ(*ex, token);
     }
   }
 }
