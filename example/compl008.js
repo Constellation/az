c43d5ad108992e43a41e8b46437187e3a87baf97
Constellation <utatane.tea@gmail.com>
2011-08-31 16:45:07 +0900

    add example/compl008.js

---
+++ example/compl008.js
@@ -0,0 +1,18 @@
+var utatane = {};
+/**
+ * @constructor
+ */
+utatane.tea = function() { }
+
+/**
+ * @return {string}
+ */
+utatane.tea.prototype.getValue = function() { }
+
+/**
+ * @type {utatane.tea}
+ */
+var val;
+
+
+val.getValue()
